8a87bd840e8b32329a29a060ed9bcbdc7a6aca92
kingabreel <gabrielyury441@gmail.com>
2023-09-19 23:55:06 -0300

    animacao

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
 
+  currentSection = 0;
+  lastSection = 0;
+
+  selectSection(index: number) {
+    this.lastSection = this.currentSection;
+    this.currentSection = index;
+  }
 }
