7935d09961a0a8e105484eca34fc596ef0d06016
kingabreel <gabrielyury441@gmail.com>
2023-09-20 13:47:54 -0300

    rodape, frames, correcao de tamanho

--- src/app/components/rodape/rodape.component.ts
+++ src/app/components/rodape/rodape.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-rodape',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./rodape.component.css']
 })
 export class RodapeComponent {
+  @ViewChild('spriteImage') spriteImage!: ElementRef;
+  feedbackGive:boolean=false
 
+  constructor(private renderer: Renderer2){}
+
+  onMouseOverEsq() {
+    this.renderer.setAttribute(this.spriteImage.nativeElement, 'src', 'assets/dedoEsq.png');
+  }
+  onMouseOverDir() {
+    this.renderer.setAttribute(this.spriteImage.nativeElement, 'src', 'assets/dedoDir.png');
+  }
+  onMouseOut() {
+    this.renderer.setAttribute(this.spriteImage.nativeElement, 'src', 'assets/paradoC.gif');
+  }
+  onClickFunc(){
+    this.feedbackGive = true
+  }
 }
