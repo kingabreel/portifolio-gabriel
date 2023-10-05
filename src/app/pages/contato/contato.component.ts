2f22fe0181307eb58a0f710c73e84e5f002cd6a9
kingabreel <gabrielyury441@gmail.com>
2023-10-05 18:50:02 -0300

    contato responsivo

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
-  styleUrls: ['./contato.component.css']
+  styleUrls: ['./contato.component.css', 'contato-responsivo.component.css']
 })
 export class ContatoComponent {
 
