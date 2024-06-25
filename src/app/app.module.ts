b71b6dbe3f24f5bee2b75fb93c4d941d32bb1811
kingabreel <gabrielyury441@gmail.com>
2024-06-25 18:35:48 -0300

    style: new style implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ItemListComponent } from './components/item-list/item-list.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
-import { TituloParagrafoComponent } from './components/titulo-paragrafo/titulo-paragrafo.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { SkillBarComponent } from './components/skill-bar/skill-bar.component';
-import { ProjetosCardComponent } from './components/projetos-card/projetos-card.component';
-import { CorpoDestaqueComponent } from './components/corpo-destaque/corpo-destaque.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
@@ -21,13 +14,6 @@ import { ContactsComponent } from './pages/contacts/contacts.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    ItemListComponent,
-    PerfilComponent,
-    TituloParagrafoComponent,
-    SkillsComponent,
-    SkillBarComponent,
-    ProjetosCardComponent,
-    CorpoDestaqueComponent,
     HomeComponent,
     AboutComponent,
     ProjectsComponent,
